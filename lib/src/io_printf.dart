f51f72a325a4a38e26412848318a3222b701e80b
riclc <riclc@hotmail.com>
2026-09-16 10:32:14 -0300

    printf sem args

--- lib/src/io_printf.dart
+++ lib/src/io_printf.dart
@@ -2,6 +2,6 @@
 // usa biblioteca: sprintf
 import 'package:sprintf/sprintf.dart';
 
-void printf(String fmt, var args) {
+void printf(String fmt, [var args = '']) {
   print( sprintf(fmt, args) );
 }
