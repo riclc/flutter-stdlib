77a69f491525fe27c27c6f0f698da926e5a2cf23
riclc <riclc@hotmail.com>
2026-09-16 10:47:23 -0300

    fix

--- lib/src/io_printf.dart
+++ lib/src/io_printf.dart
@@ -2,6 +2,6 @@
 // usa biblioteca: sprintf
 import 'package:sprintf/sprintf.dart';
 
-void printf(String fmt, [var args = []]) {
-  print( sprintf(fmt, args) );
+void printf(String fmt, [var args = null]) {
+  print( sprintf(fmt, args ?? []) );
 }
